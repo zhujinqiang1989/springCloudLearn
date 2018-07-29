86cd75eca51a6109f91feee4383ad6ec5a89d803
zhujinqiang1989 <523777698@qq.com>
2018-07-29 23:18:58 +0800

    api eureka

---
+++ api-server/src/main/java/com/zhujq/learn/api/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.zhujq.learn.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value = "name")String name){
+        return  "Hello "+ name;
+
+    }
+
+}
